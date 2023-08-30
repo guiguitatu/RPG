ce1cc37ca8fe143e5fc4adf76f7680ca7b273a91
guiguitatu <guisamfurquim@gmail.com>
2023-08-30 16:38:16 -0300

    -add images -modificação btn movimentação e css

--- rpg.js
+++ rpg.js
@@ -1,7 +1,8 @@
 const object = document.getElementById("movableObject");
 
-posxpers = 6
-posypers = 0
+d20 = false;
+posxpers = 6;
+posypers = 0;
 iniciar = false;
 inventario = [];
 mover = true;
@@ -13,8 +14,8 @@ mapa = [
     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
     [1,1,1,0,1,2,1,1,1,1,1,1,1,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,1,0,0,1,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0,2,0,0,0,5,5,0,0],
-    [0,0,1,1,0,0,0,0,0,0,0,0,2,0,0,0,5,5,0,0],
+    [0,0,8,0,0,0,0,0,0,0,0,0,2,0,0,0,5,5,0,0],
+    [0,0,1,1,0,0,0,0,8,0,0,0,2,0,0,0,5,5,0,0],
     [0,0,1,1,0,0,0,0,0,0,0,0,2,0,0,1,0,0,1,0],
     [1,1,1,1,1,2,1,1,1,1,1,1,1,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
@@ -23,15 +24,29 @@ mapa = [
 ];
 
 
-document.getElementById("btnTopLeft").addEventListener("click", () => moveObject(-50,));
-document.getElementById("btnTopRight").addEventListener("click", () => moveObject(50,));
-document.getElementById("btnBottomLeft").addEventListener("click", () => moveObject(0,-50,));
-document.getElementById("btnBottomRight").addEventListener("click", () => moveObject(0,50,));
+// document.getElementById("btnTopLeft").addEventListener("click", () => moveObject(-50,));
+// document.getElementById("btnTopRight").addEventListener("click", () => moveObject(50,));
+// document.getElementById("btnBottomLeft").addEventListener("click", () => moveObject(0,-50,));
+// document.getElementById("btnBottomRight").addEventListener("click", () => moveObject(0,50,));
+// function moveObject(x, y) {
+//   const currentTop = parseInt(object.style.top) || 0;
+//   const currentLeft = parseInt(object.style.left) || 0;
+//
+//   object.style.top = `${currentTop + y}px`;
+//   object.style.left = `${currentLeft + x}px`;
+// }
 
-function moveObject(x, y) {
-  const currentTop = parseInt(object.style.top) || 0;
-  const currentLeft = parseInt(object.style.left) || 0;
-  
-  object.style.top = `${currentTop + y}px`;
-  object.style.left = `${currentLeft + x}px`;
+function movcima(){
+    document.getElementById()
+}
+
+function movbaixo() {
+
+}
+function movesquerda(){
+
+}
+
+function movdireita() {
+
 }
